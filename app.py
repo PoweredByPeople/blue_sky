8e6a5cefeb824034003755dd04caa0791cd3d146
Louis LaCroix <lacro038@gmail.com>
2023-09-13 09:10:54 -0700

    Fix environment loading issue and working proof of concept on the API Get request.

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import requests
 import environment
 
+API_KEY=environment.APIKEY
+URL="https://ridb.recreation.gov/api/v1/"
+ENDPOINT="activities"
+headers = {'accept': 'application/json', 'apikey': API_KEY}
+request_url = f"{URL}/{ENDPOINT}"
+
 
 def main():
-
-    API_KEY=environment.APIKEY
-    URL="https://ridb.recreation.gov/api/v1/"
-    ENDPOINT="activities"
-    headers = {"accept": "application/json", "apikey": API_KEY}
-
-    request_url = f"{URL}/{ENDPOINT}"
-
-    response = requests.get(request_url, headers=headers, params={"limit":50, "offset": 5})
-
-    print(response.text) 
+    response = requests.get(request_url, headers=headers, params={"limit":50, "offset":0})
+    print(response.json())
 
 main()
--- environment.py
+++ environment.py
@@ -1,3 +1,5 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 APIKEY = os.environ.get("APIKEY")
